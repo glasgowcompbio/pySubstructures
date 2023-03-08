466a5721e0b4c042fa5b85b7303113ff0ef721b6
Jonge <niek.dejonge@wur.nl>
2023-03-08 11:57:14 +0100

    Move common and constants to ms2lda to prevent import errors

--- ms2lda/lda_variational.py
+++ ms2lda/lda_variational.py
@@ -6,7 +6,7 @@ from scipy.special import polygamma as pg
 from scipy.special import psi as psi
 from tqdm import trange
 
-from pySubstructures.constants import SMALL_NUMBER
+from constants import SMALL_NUMBER
 from lda_evaluation import compute_overlap_scores_from_dict
 
 
